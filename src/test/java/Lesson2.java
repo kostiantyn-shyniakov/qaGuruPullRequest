f3d2be484139e290d4f5b56df6428b98a2fdd9d2
kostiantyn-shyniakov <shinyakov@gmail.com>
2021-02-25 16:15:34 +0200

    added allure, selenoid and faker

--- src/test/java/Lesson2.java
+++ src/test/java/Lesson2.java
@@ -1,42 +1,52 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Lesson2 {
-
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
     }
 
     @Test
     void practiceFormTest() {
 
-        String firstName = "FirstName";
-        String lastName = "lastName";
-
         open("https://demoqa.com/automation-practice-form");
+        String[] months = {"January", "February", "March", "April", "May", "June", "July","August","September", "October", "November", "December"};
+        Faker faker = new Faker();
+        String firstName = faker.name().firstName(),
+                lastName = faker.name().lastName(),
+                email = faker.internet().emailAddress(),
+                address = faker.address().fullAddress(),
+                phoneNumber = Long.toString(faker.number().numberBetween(1000000000L,9999999999L)),
+                birthYear = Integer.toString(faker.number().numberBetween(1900,2100)),
+                birthMonth = months[faker.number().numberBetween(0,11)],
+                birthDay =  Integer.toString(faker.number().numberBetween(1,19)),
+                gender = faker.demographic().sex();
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
-        $("#userEmail").setValue("aa@aa.aa");
-        $("label[for='gender-radio-1']").click();
-        $("#userNumber").setValue("0123456789");
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(phoneNumber);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionContainingText("June");
-        $(".react-datepicker__year-select").selectOptionContainingText("1977");
-        $$(".react-datepicker__week div").findBy(text("17")).click();
+        $(".react-datepicker__month-select").selectOptionContainingText(birthMonth);
+        $(".react-datepicker__year-select").selectOptionContainingText(birthYear);
+        $$(".react-datepicker__week div").findBy(text(birthDay)).click();
         $("#subjectsInput").setValue("en");
         $("#react-select-2-option-1").click();
         $("label[for='hobbies-checkbox-1']").click();
         $("label[for='hobbies-checkbox-2']").click();
         $("label[for='hobbies-checkbox-3']").click();
         $("#uploadPicture").uploadFromClasspath("picture.jpg");
-        $("#currentAddress").setValue("currentAddress here");
+        $("#currentAddress").setValue(address);
         $("#state").click();
         $("#react-select-3-option-2").click();
         $("#city").click();
@@ -44,15 +54,15 @@ public class Lesson2 {
         $("#submit").click();
 
         ElementsCollection submitElements = $$(".table tbody tr");
-        submitElements.get(0).shouldHave(text("FirstName lastName"));
-        submitElements.get(1).shouldHave(text("aa@aa.aa"));
-        submitElements.get(2).shouldHave(text("Male"));
-        submitElements.get(3).shouldHave(text("0123456789"));
-        submitElements.get(4).shouldHave(text("17 June,1977"));
+        submitElements.get(0).shouldHave(text(firstName+" "+lastName));
+        submitElements.get(1).shouldHave(text(email));
+        submitElements.get(2).shouldHave(text(gender));
+        submitElements.get(3).shouldHave(text(phoneNumber));
+        submitElements.get(4).shouldHave(text(birthDay+" "+birthMonth+","+birthYear));
         submitElements.get(5).shouldHave(text("Computer Science"));
         submitElements.get(6).shouldHave(text("Sports, Reading, Music"));
         submitElements.get(7).shouldHave(text("picture.jpg"));
-        submitElements.get(8).shouldHave(text("currentAddress here"));
+        submitElements.get(8).shouldHave(text(address));
         submitElements.get(9).shouldHave(text("Haryana Panipat"));
         $("#closeLargeModal").click();
     }
